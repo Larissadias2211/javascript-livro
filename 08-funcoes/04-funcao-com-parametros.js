ecdad9191f6ef2ad74d67ceca57434d9b9381702
larissa dias <110576062+Larissadias2211@users.noreply.github.com>
2022-12-08 16:12:17 -0300

    exercicio

--- 08-funcoes/04-funcao-com-parametros.js
+++ 08-funcoes/04-funcao-com-parametros.js
@@ -1,5 +1,5 @@
-// Declarando a função 
-function media(a, b, c, d) {
+// Declarando a função
+function Media(a, b, c, d) {
     return (a+b+c+d) /4;
 }
 
@@ -9,4 +9,14 @@ var nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0;
 
 
 // Mensagem para o usuário
-alert('informe as 4 notas do(a) aluno')
+alert('Iforme as 4 notas do(a) aluno(a)');
+
+
+// Solicitando as notas para o usuário
+nota1 = parseFloat(prompt('Digite a primeira nota'));
+nota2 = parseFloat(prompt('Digite a segunda nota'));
+nota3 = parseFloat(prompt('Digite a terceira nota'));
+nota4 = parseFloat(prompt('Digite a quarta nota'));
+
+// Mostrando o resultado 
+document.getElementById('teste').innerHTML += Media(nota1, nota2, nota3, nota4);
---
+++ 08-funcoes/06-funcao-com-parametros.js
@@ -0,0 +1,33 @@
+// Declarando a função
+function VerificaForm(frm) {
+    if(frm.nome.value == '') {
+        alert('campo obrigatório');
+        frm.nome.focus();
+        return false;
+    }
+
+    if(frm.email.value == '') {
+        alert('Campo obrigatório');
+        frm.email.focus();
+        return false;
+    }
+
+    if(frm.email.value == '') {
+        alert('Campo obrigatório');
+        frm.email.focus();
+        return false;
+    }
+    
+    if(frm.email.value == '') {
+        alert('Campo obrigatório');
+        frm.email.focus();
+        return false;
+    } 
+    
+    if(frm.email.value == '') {
+        alert('Campo obrigatório');
+        frm.email.focus();
+        return false;
+    }  
+    
+}
